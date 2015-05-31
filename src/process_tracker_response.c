e7999ecc717d564d5492542c1169c4d21ec571d5
tzzcl <zclnjucs@gmail.com>
2015-05-31 22:57:10 +0800

    > compile Linux ubuntu 3.13.0-44-generic #73-Ubuntu SMP Tue Dec 16 00:22:43 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux

--- src/process_tracker_response.c
+++ src/process_tracker_response.c
@@ -27,11 +27,10 @@ tracker_response* preprocess_tracker_response(int sockfd)
      exit(-6);
    }
    strncpy(tmp,rcvline,17);
-   
+   printf("%s:%s\n",__FUNCTION__,tmp);
    if(strncmp(tmp,"HTTP/1.0 200 OK\r\n",strlen("HTTP/1.0 200 OK\r\n")))
    {
      perror("Error, didn't match HTTP line");
-     puts("AAA");
      exit(-6);
    }
    memset(rcvline,0xFF,MAXLINE);

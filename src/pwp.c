c05ca315384a82ff206d85ed231910254b9000f9
tzzcl <zclnjucs@gmail.com>
2015-06-04 13:10:25 +0800

    > compile Linux ubuntu 3.13.0-44-generic #73-Ubuntu SMP Tue Dec 16 00:22:43 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux

--- src/pwp.c
+++ src/pwp.c
@@ -9,3 +9,49 @@
 #include <errno.h>
 #include <unistd.h>
 #include <pthread.h>
+#define DEBUG(x) x
+ListHead p2p_cb_head;
+ListHead download_piece_head;
+int first_req=1;
+int* piece_count;
+pthread_mutex_t p2p_mutex;
+pthread_mutex_t download_mutex;
+pthread_mutex_t first_req_mutex;
+pthread_mutex_t piece_count_mutex;
+
+static int readn(int fd,void* content,size_t len)
+{
+	int state=0;
+	int cnt=len;
+	while (cnt>0){
+		state=recv(fd,content,cnt,0);
+		if (state<0)
+		{
+			if (errno==EINTR)
+				continue;
+			return -1;
+		}
+		if (state==0)
+		{
+			return len-cnt;
+		}
+		content+=state;
+		cnt-=state;
+	}
+	return len;
+}
+
+static inline int get_bit_at_index(char *bitfield, int index){
+    unsigned char ch = bitfield[index/8];
+    int offset = 7 - index%8;
+    return (ch >> offset) & 1;
+}
+
+static inline void set_bit_at_index(char *bitfield, int index, int bit){
+    assert(bit == 0 || bit == 1);
+    int offset = 7 - index%8;
+    if(bit)
+        bitfield[index/8] = bitfield[index/8] | set_bit[offset];
+    else
+        bitfield[index/8] = bitfield[index/9] & (~set_bit[offset]);
+}

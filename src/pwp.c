cc61c26d45e3a4b1b608d73c1ee16ddcce9fb3ad
tzzcl <zclnjucs@gmail.com>
2015-06-08 10:21:17 +0800

    > compile Linux ubuntu 3.13.0-44-generic #73-Ubuntu SMP Tue Dec 16 00:22:43 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux

--- src/pwp.c
+++ src/pwp.c
@@ -86,3 +86,75 @@ int listen_init(){
 	listen(listenfd,8);
 	return listenfd;
 }
+
+static inline int is_bitfield_empty(char *bitfield,int len){
+    int flag = 1;
+    for(int i = 0; i < len; i++){
+        if(bitfield[i] != 0){
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+
+int valid_ip(char* ip){
+	ListHead* ptr;
+	pthread_mutex_lock(&p2p_mutex);
+	list_foreach(ptr,&p2p_cb_head){
+		p2p_cb *temp = list_entry(ptr,p2p_cb,list);
+		if (strcmp(temp->peer_ip,ip)==0)
+		{
+			pthread_mutex_unlock(&p2p_mutex);
+			return 1;
+		}
+	}
+	pthread_mutex_unlock(&p2p_mutex);
+	return 0;
+}
+p2p_cb* new_init_p2p(){
+	p2p_cb* temp=malloc(sizeof(p2p_cb));
+	memset(temp,0,sizeof(p2p_cb));
+	temp->self_choke=1;
+	temp->self_interest=0;
+	temp->peer_choke=1;
+	temp->peer_interest=0;
+	list_init(&temp->list);
+	return temp;
+}
+
+void* p2p_run_thread(void* param){
+	p2p_thread *current = (p2p_thread *)param;
+
+}
+void send_request(int connfd,int index,int begin,int length){
+
+}
+void send_interest(int connfd){
+
+}
+void send_choke(int connfd){
+
+}
+void send_not_interest(int connfd){
+
+}
+void send_unchoke(int connfd){
+
+}
+void send_piece(int connfd,int index,int begin,int length){
+
+}
+void send_handshake(int connfd){
+	char* pstr="BitTorrent protocol";
+	const int pstrlen=19;
+	const int len=49+pstrlen;
+	char msg[len];
+	memset(msg,0,sizeof(msg));
+	char* content=msg;
+	content[0]=19;
+	content++;
+	memcpy(content,pstr,sizeof(char)*pstrlen);
+	//to do global hash
+	send(connfd,msg,len,0);
+}

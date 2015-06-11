a7dbb875ca5c7fd9a793e62a0b223a47d9c1f56d
tzzcl <zclnjucs@gmail.com>
2015-06-11 15:48:52 +0800

    > compile Linux ubuntu 3.13.0-44-generic #73-Ubuntu SMP Tue Dec 16 00:22:43 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux

--- src/file.c
+++ src/file.c
@@ -1,8 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <arpa/inet.h>
 #include "file.h"
-
+#include "sha1.h"
+#include "info.h"
 char* get_block(int index){
 	return NULL;
 }
 void set_block(int index,int begin,int length,char* block){
-	
+
 }

34d2c5c4dfc27d2ea58d6c4df3bbda16a4241e6a
tzzcl <zclnjucs@gmail.com>
2015-06-12 14:46:15 +0800

    > compile Linux ubuntu 3.13.0-44-generic #73-Ubuntu SMP Tue Dec 16 00:22:43 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux

--- src/parsetorrentfile.c
+++ src/parsetorrentfile.c
@@ -136,10 +136,10 @@ torrentmetadata_t* parsetorrentfile(char* filename)
         }
 
       } // for循环结束
+	  printf("%p\n",idict[j].key);
       for (j = 0; idict[j].key != NULL; j++){
                 if (!strcmp(idict[j].key, "files")){
                     assert(idict[j].val->type == BE_LIST);
-					puts("AAA");
                     int k;
                     for(k = 0; idict[j].val->val.l[k] != NULL; k++){
                         struct be_dict *filedict = idict[j].val->val.l[k]->val.d;

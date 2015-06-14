032171df69215e6bb9e448a77910007a6787a27f
tzzcl <zclnjucs@gmail.com>
2015-06-14 14:51:06 +0800

    > compile Linux ubuntu 3.13.0-44-generic #73-Ubuntu SMP Tue Dec 16 00:22:43 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux

--- src/pwp.c
+++ src/pwp.c
@@ -630,6 +630,7 @@ void* p2p_run_thread(void* param){
                         						int begin1,length1;
                         						if (!select_next_subpiece(next_index,&begin1,&length1))
                         							no_sub_piece=1;
+                        						printf("%s:%d\n","temp->connfd",temp->connfd);
                         						send_request(temp->connfd,next_index,begin1,length1);
                         						next_d_piece->download_num++;
                         						int subpiece_index=begin1/next_d_piece->sub_piece_size;

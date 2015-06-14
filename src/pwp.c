2ad3d093dae3dc8d71c1d8b765d28353b2bb0150
tzzcl <zclnjucs@gmail.com>
2015-06-14 14:23:40 +0800

    > compile Linux ubuntu 3.13.0-44-generic #73-Ubuntu SMP Tue Dec 16 00:22:43 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux

--- src/pwp.c
+++ src/pwp.c
@@ -456,7 +456,6 @@ void* p2p_run_thread(void* param){
 					newcb->peer_interest=1;
 					newcb->self_choke=0;
 					pthread_mutex_lock(&first_req_mutex);
-					puts("AAA");
 					if (first_req==1)
 					{
 						pthread_mutex_unlock(&first_req_mutex);
